b2d2fe955e7d97c3cf7655d23afd4f82298244f5
kingspin-sudo <workwithwaqar@gmail.com>
2023-01-21 17:12:35 +0500

    First Commit

---
+++ app/src/main/java/com/codecademy/mydailydiary/MainActivity.java
@@ -0,0 +1,20 @@
+package com.codecademy.mydailydiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG,"I am a logging pro!");
+        Log.i(TAG,"again I am a logging pro!");
+
+
+    }
+}
